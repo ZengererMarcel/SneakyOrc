8b35999757dd3fd1e5b5840340ad53bc935dc1f9
ZengererMarcel <marcel5699@edu.fh-joanneum.at>
2020-12-16 18:25:35 +0100

    - Split the values from url - Generate email from firstname and lastname - Add the functionality for the addToDo Button

---
+++ main/src/js/AddData.js
@@ -0,0 +1,26 @@
+
+window.addEventListener("load", function() {
+    var getParams = function (url) {
+        var params = {};
+        var parser = document.createElement('a');
+        parser.href = url;
+        var query = parser.search.substring(1);
+        if (query.includes('&')) {
+            var vars = query.split('&');
+            for (var i = 0; i < vars.length; i++) {
+                var pair = vars[i].split('=');
+                params[pair[0]] = decodeURIComponent(pair[1]);
+            }
+            return params;
+        } else {
+            return null;
+        }
+    };
+
+    var params = getParams(window.location.href);
+
+
+});
+
+
+
---
+++ main/src/js/AddToDo.js
@@ -0,0 +1,29 @@
+var toDoNumber = 1;
+
+function addToDo(){
+    toDoNumber += 1;
+    var divToDo = document.getElementById("divToDo");
+
+    var label = document.createElement("label");
+    label.className = "control-label col-sm-2";
+    label.htmlFor =`toDo${toDoNumber}`;
+    divToDo.appendChild(label);
+
+    var div = document.createElement("div");
+    div.className = "col-sm-10";
+
+    var input = document.createElement("input");
+    input.className = "form-control";
+    input.id = `toDo${toDoNumber}`;
+    input.placeholder = "Was muss gemacht werden?"
+    input.name = `toDo${toDoNumber}`;
+    div.appendChild(input);
+
+    divToDo.appendChild(div);
+}
+
+function setEmail(){
+    var email = document.getElementById("firstName").value + "." + document.getElementById("lastName").value + "@web.com";
+    document.getElementById("email").value = email;
+    console.log(email)
+}
