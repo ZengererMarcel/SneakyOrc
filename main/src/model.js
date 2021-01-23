69ad9fa2ef4698e42bc608595504e8c78990e8d9
ZengererMarcel <marcel5699@edu.fh-joanneum.at>
2021-01-23 20:09:31 +0100

    Fix problem where string with a space switch it to a plus

--- main/src/model.js
+++ main/src/model.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 
 // first we will need a connection, use app-*.sql files to create database and table, and to insert first demo data
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dbd_webtech",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dbd_webtech",
 });
 
 var toDos = [];
@@ -25,81 +25,85 @@ connection.connect();
 
 // get all milestone
 function getAll() {
-  // createa new Promise object, which have to major status
-  // resolve -> success -> we will get result set(s)
-  // reject  -> error   -> we are currently log errors in console and reject this error
-  return new Promise((resolve, reject) => {
-    // create query
+    // createa new Promise object, which have to major status
+    // resolve -> success -> we will get result set(s)
+    // reject  -> error   -> we are currently log errors in console and reject this error
+    return new Promise((resolve, reject) => {
+        // create query
 
-    const query = "SELECT * FROM employee_disp";
-    // do query, and wait for resolve or reject
-    connection.query(query, (error, results) => {
-      if (error) {
-        console.log(error);
-        return reject(error);
+        const query = "SELECT * FROM employee_disp";
+        // do query, and wait for resolve or reject
+        connection.query(query, (error, results) => {
+            if (error) {
+                console.log(error);
+                return reject(error);
 
-        // there's usually no error, so we are able to use result (set)
-      } else {
-        resolve(results);
-      }
+                // there's usually no error, so we are able to use result (set)
+            } else {
+                resolve(results);
+            }
+        });
     });
-  });
 }
 
 // insert a new milestone
 function save(employee) {
 
-      var getParams = function (url) {
+    var getParams = function (url) {
         params = {};
-          var vars = url.split("&")
-          toDONumber = vars.length - 6;
-          for (var i = 0; i < vars.length; i++) {
+        var vars = url.split("&")
+        toDONumber = vars.length - 6;
+        for (var i = 0; i < vars.length; i++) {
             var pair = vars[i].split('=');
             params[pair[0]] = decodeURIComponent(pair[1]);
-          }
-          return params;
-      }
+        }
+        return params;
+    }
 
-      params = getParams(employee);
-      var email = params["firstName"] + "." + params["lastName"] + "@web.com";
-      email = email.toLowerCase();
+    params = getParams(employee);
 
-  return new Promise((resolve, reject) => {
+    filterString("firstName");
+    filterString("lastName");
 
-      getDepartmentId(params["department"]).then(
-          (Id) => {
-              departmentId = Id[0].department_id;
-          },
-          (error) => {
-              console.log(error);
-          }
-      )
+    var email = params["firstName"] + "." + params["lastName"] + "@web.com";
+    email = email.toLowerCase();
 
-      getStatusId(params["status"]).then(
-          (Id) => {
-              statusId = Id[0].status_id;
-              insertEmployee(params["firstName"],
-                  params["lastName"],
-                  params["birthday"],
-                  params["phoneNumber"],
-                  email,
-                  departmentId,
-                  statusId).then(
-                  (abc) => {
-                      makeToDo();
-                      resolve(abc);
-                  },
-                  (error) => {
-                      console.log(error);
-                      reject(error);
-                  }
-              )
-          },
-          (error) => {
-              console.log(error);
-          }
-      )
-  });
+    return new Promise((resolve, reject) => {
+
+        getDepartmentId(params["department"]).then(
+            (Id) => {
+                departmentId = Id[0].department_id;
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+
+        getStatusId(params["status"]).then(
+            (Id) => {
+                statusId = Id[0].status_id;
+                insertEmployee(params["firstName"],
+                    params["lastName"],
+                    params["birthday"],
+                    params["phoneNumber"],
+                    email,
+                    departmentId,
+                    statusId).then(
+                    (abc) => {
+                        makeToDo();
+                        resolve(abc);
+                    },
+                    (error) => {
+                        console.log(error);
+                        reject(error);
+                    }
+                )
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    });
 }
 
 function makeToDo() {
@@ -123,7 +127,7 @@ function makeToDo() {
         toDos[i] = params["toDo" + (i + 1)];
     }
 
-    if(toDos[0].length != 0) {
+    if (toDos[0].length != 0) {
         insertToDo().then(
             (resolve) => {
             },
@@ -143,7 +147,7 @@ function makeToDo() {
     }
 }
 
-function insertEmployee(first, last, birth, phone, email, depart, status){
+function insertEmployee(first, last, birth, phone, email, depart, status) {
     return new Promise((resolve, reject) => {
         const query =
             "INSERT INTO employee (first_name, last_name, birth_date, phone_number, email, department_id, status_id) VALUES (?,?,?,?,?,?,?)";
@@ -164,12 +168,12 @@ function insertEmployee(first, last, birth, phone, email, depart, status){
     });
 }
 
-function insertPersonToDo(){
+function insertPersonToDo() {
     return new Promise((resolve, reject) => {
         let query =
             "INSERT INTO persontodo(employee_id, toDo_id) SELECT MAX(a.employee_id) AS employee_id, b.toDo_id FROM employee a, todo b WHERE b.toDo_name = ?";
 
-        for(let i = 1; i < toDONumber; i++){
+        for (let i = 1; i < toDONumber; i++) {
             query = query + " OR b.toDo_name = ?";
         }
 
@@ -190,12 +194,12 @@ function insertPersonToDo(){
     });
 }
 
-function insertToDo(){
+function insertToDo() {
     return new Promise((resolve, reject) => {
         let query =
             "INSERT IGNORE INTO todo (toDo_name) VALUES (?)";
 
-        for(let i = 1; i < toDONumber; i++){
+        for (let i = 1; i < toDONumber; i++) {
             query = query + ",(?)";
         }
         connection.query(
@@ -213,7 +217,7 @@ function insertToDo(){
     });
 }
 
-function getDepartmentId(department){
+function getDepartmentId(department) {
     return new Promise((resolve, reject) => {
         const query = "SELECT department_id FROM departments WHERE department_name = ?";
         connection.query(query, [department,], (error, results) => {
@@ -226,7 +230,7 @@ function getDepartmentId(department){
     });
 }
 
-function getStatusId(status){
+function getStatusId(status) {
     return new Promise((resolve, reject) => {
         const query = "SELECT status_id FROM status WHERE status_description = ?";
         connection.query(query, [status,], (error, results) => {
@@ -239,9 +243,24 @@ function getStatusId(status){
     });
 }
 
+function filterString(columnName){
+    var word = params[columnName];
+
+    var copyToDO = "";
+    for (var i = 0; i < word.length; i++) {
+        if (word[i] == "+") {
+            copyToDO = copyToDO + " ";
+        } else {
+            copyToDO = copyToDO + word[i];
+        }
+    }
+
+    params[columnName] = copyToDO;
+}
+
 module.exports = {
-  getAll,
-  save(url){
-      return save(url);
-  },
+    getAll,
+    save(url) {
+        return save(url);
+    },
 };
